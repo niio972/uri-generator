b194ead20cad8a470635560a612929fb31be2828
Jean-Eudes Hollebecq <jehollebecq@gmail.com>
2020-03-16 09:43:18 +0100

    new_URI design

--- static/js/appVue.js
+++ static/js/appVue.js
@@ -2,11 +2,25 @@ var app = new Vue({
   el: '#URI_generator',
   data: {
     URI: 'your uri model',
-    key_class: "primary_key"
+    key_class: "primary_key",
+    segments: [ ]
   },
   delimiters: ['[[',']]'],
-  methods: {
-    
+  methods:{
+    GetCellValues: function() {
+        var self=this
+        var table = document.getElementById('mytable');
+        for (var r = 1, n = table.rows.length; r < n; r++) {
+            self.segments.push(table.rows[r].cells[1].innerHTML)
+        }
+      }
+    },
+  computed: {
+    fill_URI: function(){
+      var self=this;
+      self.URI = "hostname:"+self.segments.join("/");
+      return(self.URI)
+    }  
     
   }
 })

2c39293582bd3b9169e95340be83132d3d1c32d9
Jean-Eudes Hollebecq <jehollebecq@gmail.com>
2020-07-27 11:42:19 +0200

    improve menu labels

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
-    # password = db.Column(db.String)
     password_hash =db.Column(db.String)
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
